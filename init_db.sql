7442c4f7a4bb8a7c7b6cf5cbc25c184d937f0a3e
Igor-Polatajko <igor.bogdanovich39@gmail.com>
2019-08-30 23:32:38 +0300

    Fixed travis and maven

--- init_db.sql
+++ init_db.sql
@@ -1,4 +1,9 @@
-CREATE TABLE `booking_app`.`users` (
+-- Creating DB
+CREATE DATABASE IF NOT EXISTS `booking_app` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `booking_app`;
+
+-- Initializing Users table
+CREATE TABLE IF NOT EXISTS `Users` (
   `id` VARCHAR(45) NOT NULL,
   `password` VARCHAR(45) NOT NULL,
   `name` VARCHAR(45) NOT NULL,
@@ -8,8 +13,5 @@ CREATE TABLE `booking_app`.`users` (
   `active` VARCHAR(45) NOT NULL,
   `created_date` DATETIME NULL,
   `updated_date` DATETIME NULL,
-  PRIMARY KEY (`id`));
-
-ALTER TABLE `booking_app`.`users`
-ADD UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE;
-;
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC));
